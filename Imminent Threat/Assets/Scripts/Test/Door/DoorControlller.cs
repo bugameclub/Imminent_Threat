d0a043e0b58de19b2989eeed30c5fa4da2e08eda
BU Game Design <bugamedesignclub@gmail.com>
2022-10-29 15:28:43 -0400

    Started Tutorial
    Setting up tutorial

---
+++ Imminent Threat/Assets/Scripts/Test/Door/DoorControlller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControlller : MonoBehaviour
+{
+    // Start is called before the first frame update
+	
+    void Start()
+    {
+        //does somethin
+		Debug.Log("odfosdnf");
+		//hey this code does thi s or that
+		
+		
+    }
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+        
+    }
+}
