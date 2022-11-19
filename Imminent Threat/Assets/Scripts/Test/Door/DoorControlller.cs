f117b1caffc29df1616f2e04b2be23ba7534909f
BU Game Design <bugamedesignclub@gmail.com>
2022-11-19 15:15:13 -0500

    Test Demo Update
    Sample demo of door opening!
    
    Check it out and tinker around!

--- Imminent Threat/Assets/Scripts/Test/Door/DoorControlller.cs
+++ Imminent Threat/Assets/Scripts/Test/Door/DoorControlller.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class DoorControlller : MonoBehaviour
 {
     // Start is called before the first frame update
-	
+	bool is_at_door;
+	Animator m_Animator;
+	public LayerMask interactableLayer;
     void Start()
     {
+		 m_Animator = GetComponent<Animator>();		 
         //does somethin
-		Debug.Log("odfosdnf");
-		//hey this code does thi s or that
+		//Debug.Log("Hello");
+		//hey this code does this or that
 		
 		
     }
@@ -19,7 +22,26 @@ public class DoorControlller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	
+		if (Input.GetKeyDown(KeyCode.E) && is_at_door)
+		{
+			Debug.Log("Door must open!!");
+			m_Animator.SetBool("isopen", true);
+		}
         
+    }
+	
+	private void OnTriggerEnter(Collider other)
+    {
+		
+		if(1 << other.gameObject.layer == interactableLayer.value){
+			is_at_door = true;
+		}
+    }
+	
+	void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.name == "FPSController"){
+			is_at_door = false;
+		}
     }
 }
